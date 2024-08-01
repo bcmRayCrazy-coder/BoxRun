9802023a57981f5edd51fd62fa16caaafec4aacb
bcmray_crazy <975875040@qq.com>
2024-08-01 22:20:39 +0800

    添加旗帜动画，修复ui显示问题

--- assets/Script/UiActiveController.ts
+++ assets/Script/UiActiveController.ts
@@ -1,4 +1,4 @@
-import { _decorator, CCString, Component, Node } from 'cc';
+import { _decorator, CCBoolean, CCString, Component, Node } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('UiActiveController')
@@ -6,8 +6,12 @@ export class UiActiveController extends Component {
     @property(CCString)
     uiName: String = 'No Name';
 
+    @property(CCBoolean)
+    defaultActive: boolean = true;
+
     start() {
         this.node.on('uiActive', this.uiActive, this);
+        this.node.active = this.defaultActive;
     }
 
     uiActive(name: string, active: boolean) {
